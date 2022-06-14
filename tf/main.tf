bbf773be5c312a70cf3f47dc7760baeac73ce712
kylep3 <kyle@pericak.com>
2022-06-13 21:06:10 -0400

    added ec2 iam role to tf

--- tf/main.tf
+++ tf/main.tf
@@ -10,5 +10,38 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ca-central-1"
+}
+
+resource "aws_iam_role" "kytrade2-EKS-Cluster-Role" {
+    arn                   = "arn:aws:iam::883546544043:role/kytrade2-EKS-Cluster-Role"
+    assume_role_policy    = jsonencode(
+        {
+            Statement = [
+                {
+                    Action    = "sts:AssumeRole"
+                    Effect    = "Allow"
+                    Principal = {
+                        Service = "eks.amazonaws.com"
+                    }
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+    create_date           = "2022-06-12T20:21:40Z"
+    description           = "Allows access to other AWS service resources that are required to operate clusters managed by EKS."
+    force_detach_policies = false
+    id                    = "kytrade2-EKS-Cluster-Role"
+    managed_policy_arns   = [
+        "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    ]
+    max_session_duration  = 3600
+    name                  = "kytrade2-EKS-Cluster-Role"
+    path                  = "/"
+    tags                  = {}
+    tags_all              = {}
+    unique_id             = "AROA43N32K6VYZLXGDUQA"
+
+    inline_policy {}
 }
